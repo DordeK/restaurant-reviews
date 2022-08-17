af824f5a499c0f4c9742c565af90d68e4b85d8a2
Dorde <dorde.kremenovic@dlabs.si>
2022-08-18 01:22:32 +0200

    remove build folder

--- BE/public/users/users.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserService } from './users.service';
-export declare class UsersController {
-    private readonly usersService;
-    constructor(usersService: UserService);
-}
--- BE/public/users/users.service.d.ts
+++
@@ -1,5 +0,0 @@
-import { User } from './entities/user.entity';
-export declare class UserService {
-    findByEmail(email: string): Promise<User | undefined>;
-    findOne(id: string): Promise<User | undefined>;
-}
