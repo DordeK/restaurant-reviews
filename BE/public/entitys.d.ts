9171c67cefec1b65db00d31d477ddef3fa7acb7c
Dorde <dorde.kremenovic@dlabs.si>
2022-08-18 00:27:18 +0200

    remove build folder

--- BE/public/entitys.d.ts
+++
@@ -1,9 +0,0 @@
-declare class RefreshToken {
-    constructor(init?: Partial<RefreshToken>);
-    id: number;
-    userId: number;
-    userAgent: string;
-    ipAddress: string;
-    sign(): string;
-}
-export default RefreshToken;
