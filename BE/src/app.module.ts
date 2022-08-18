65f238b15e9ec26dde54b790c768d7d9c0382f40
Dorde <dorde.kremenovic@dlabs.si>
2022-08-18 14:29:49 +0200

    conneect controllers from all be's, change vercel config

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { UserService } from './users/users.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController, UsersController],
+  providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
